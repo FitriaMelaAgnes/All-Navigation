8eb8f59062465bb3cc2cc0c185600020a340a11f
Agnes-PC <fitria.mela9a17@gmail.com>
2017-06-07 22:43:13 +0700

    all done

---
+++ app/src/main/java/xir3n12/remidiuts/telkomschid/allnavigation/nav1aFragment.java
@@ -0,0 +1,29 @@
+package xir3n12.remidiuts.telkomschid.allnavigation;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class nav1aFragment extends Fragment {
+
+
+    public nav1aFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_nav1a, container, false);
+    }
+
+}
